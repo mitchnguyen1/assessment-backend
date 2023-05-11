db36ce4c5394bd362715d5f12af0be192ffa1877
Mitchell Nguyen <mitch01nguyen@gmail.com>
2023-05-11 14:04:27 -0500

    adds working function for frontend

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 
 app.use(express.json());
-
+app.use(express.static(`${__dirname}/../client`))
 
 const { getCompliment,getFortune,newGoal,updateGoal,deleteGoal,displayGoals } = require('./controller')
 
